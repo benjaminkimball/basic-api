6151e3d72d46026973ce025d6b06f62c22c9f4bd
Benjamin Kimball <hello@benjaminkimball.com>
2023-05-06 14:03:42 -0600

    Refine dotenv example

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -1,9 +1,9 @@
 import type { User } from "@prisma/client";
 import { pbkdf2Sync, randomBytes } from "crypto";
 
-const digest = process.env.PWD_DIGEST || "sha512";
+const digest = process.env.PWD_HASH_DIGEST || "sha512";
 const interations = parseInt(process.env.PWD_HASH_ITERATIONS || "1000");
-const keylen = parseInt(process.env.PWD_KEY_LENGTH || "64");
+const keylen = parseInt(process.env.PWD_HASH_KEY_LENGTH || "64");
 
 function hashSync(plaintext: string, salt: string): string {
   return pbkdf2Sync(plaintext, salt, interations, keylen, digest).toString(
